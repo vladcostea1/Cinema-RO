f6fd0024e2f933c63e8207f1a5e7a6a3a04f616d
Vlad Costea <vbodescu314@gmail.com>
2026-08-01 09:37:28 +0300

    hosting cinemaRo

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    allowedHosts: ["cinema-ro-production.up.railway.app"]
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
