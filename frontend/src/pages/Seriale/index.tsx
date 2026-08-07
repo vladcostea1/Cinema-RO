f14b819826c879dd3039b2b6e618b8d900f47ec4
Vlad Costea <vbodescu314@gmail.com>
2026-08-07 11:01:49 +0300

    react

--- frontend/src/pages/Seriale/index.tsx
+++ frontend/src/pages/Seriale/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import{ useEffect, useState } from "react";
 import UniversalCard from "../../Components/UniversalCard/index.tsx.ts";
 import BackButton from "../../Components/BackButton/index.tsx";
 type Film = {
