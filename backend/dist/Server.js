34fe614a273e64257369edeb209e2da8a822f52f
Vlad Costea <vbodescu314@gmail.com>
2026-08-07 13:10:36 +0300

    Fix backend config

---
+++ backend/dist/Server.js
@@ -0,0 +1,46 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const db_1 = require("./db");
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.get("/", (req, res) => {
+    res.send("Serverul este pornit");
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serverul ruleaza pe http://localhost:${PORT}`);
+});
+app.get("/api/filme", async (req, res) => {
+    try {
+        const result = await db_1.pool.query("SELECT * FROM filme ORDER BY id");
+        res.json(result.rows);
+    }
+    catch (error) {
+        console.error("EROARE SQL:", error);
+        res.status(500).json({
+            error: "Eroare la obținerea filmelor",
+            details: String(error),
+        });
+    }
+});
+app.get("/api/seriale", async (req, res) => {
+    try {
+        const resault = await db_1.pool.query("SELECT * FROM seriale ORDER BY id");
+        res.json(resault.rows);
+    }
+    catch (error) {
+        console.error("EROARE:", error);
+        res.status(500).json({
+            error: "Eroare la obținerea serialelor",
+            details: String(error),
+        });
+    }
+});
---
+++ backend/dist/db.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.pool = void 0;
+const pg_1 = require("pg");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+exports.pool = new pg_1.Pool({
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+});
+exports.pool
+    .connect()
+    .then(() => console.log("Conectat cu succes"))
+    .catch((err) => console.error("Eroare la conectare:", err));
